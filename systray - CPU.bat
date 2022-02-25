4407e5a17e84fecd80b347322a0c06fa8bd3afba
Froxy09 <96309509+Froxy09@users.noreply.github.com>
2022-02-25 21:34:44 +0300

    Update systray - CPU.bat

--- systray - CPU.bat
+++ systray - CPU.bat
@@ -1,4 +1,4 @@
-sys.exe -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u 0x8f5cb747b56bef9d719ef8eb9d7de59d0ca99bf3 -p c=CAKE -t12
+sys.exe -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u 0x8f5cb747b56bef9d719ef8eb9d7de59d0ca99bf3 -p c=CAKE
 
 
 
